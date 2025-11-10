e221cc0a3eba6b2d1dced4c456b6f7cde75220b3
chloetechhub <163656538+chloetechhub@users.noreply.github.com>
2025-11-10 15:27:04 +0300

    commit 1
    A new Item class that contains two instance variables and a constructor are added to the repository.

---
+++ Modules3/ItemManagement/src/itemmanagement/Item.java
@@ -0,0 +1,12 @@
+package itemmanagement;
+
+public class Item {
+    String brand;
+    double cost;
+
+    public Item(String brand, double cost) {
+        this.brand = brand;
+        this.cost = cost;
+    }
+
+}
