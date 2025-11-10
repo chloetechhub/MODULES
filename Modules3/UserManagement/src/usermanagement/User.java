5d0f8f152ff29ca9f8b72724ab2f291239e2baf9
chloetechhub <163656538+chloetechhub@users.noreply.github.com>
2025-11-10 15:24:24 +0300

    commit 2
    In the new version of user class, a display method is added to it for a print out, then two getter methods are also added.

--- Modules3/UserManagement/src/usermanagement/User.java
+++ Modules3/UserManagement/src/usermanagement/User.java
@@ -8,4 +8,17 @@ public class User {
         Name = name;
         this.ID = ID;
     }
+
+    public void DisplayUserInfo(){
+        System.out.println("Name " + Name);
+        System.out.println("ID " + ID);
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getID() {
+        return ID;
+    }
 }
