ed80a976eaf16573286407c7a90b9f92c847aad8
chloetechhub <163656538+chloetechhub@users.noreply.github.com>
2025-11-10 15:42:48 +0300

    commit 2
    A new main class is created. It imports the packages of item and user then two objects are created adding in values to the instance variables of the two classes.

---
+++ Modules3/MainManagement/src/mainmanagement/Main.java
@@ -0,0 +1,10 @@
+package mainmanagement;
+import itemmanagement.Item;
+import usermanagement.User;
+public class Main {
+    public static void main(String[] args) {
+        User us = new User("Singa",2);
+        Item it = new Item("Bread",200);
+
+       
+}
